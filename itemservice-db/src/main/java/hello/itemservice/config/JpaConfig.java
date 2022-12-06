1238d51183e847dc85b09f5c4933464a6944c0c7
seongwoon jeong <ggogumalove@gmail.com>
2022-12-07 08:39:01 +0900

    [feat] add jpa example

--- itemservice-db/src/main/java/hello/itemservice/config/JpaConfig.java
+++ itemservice-db/src/main/java/hello/itemservice/config/JpaConfig.java
@@ -1,26 +1,29 @@
-package hello.itemservice.repository.jdbctemplate;
+package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 @RequiredArgsConstructor
-public class JdbcTemplateV1Config {
-    private final DataSource dataSource;
+public class JpaConfig {
+
+    private final EntityManager em;
 
     @Bean
-    public ItemService itemService(){
+    public ItemService itemService() {
         return new ItemServiceV1(itemRepository());
     }
 
+
     @Bean
-    public ItemRepository itemRepository(){
-        return new JdbcTemplateItemRepositoryV1(dataSource);
+    public ItemRepository itemRepository() {
+        return new JpaItemRepository(em);
     }
 }
